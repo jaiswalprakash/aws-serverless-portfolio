6523b3dcd9826a8c5bec2eed3ad1bb8d3a0388dc
prakashjaiswal <prakashjaiswal625@gmail.com>
2024-03-02 09:06:49 +0530

    S3 put and delete object

--- src/AWS-S3/S3-service.js
+++ src/AWS-S3/S3-service.js
@@ -1,4 +1,4 @@
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, GetObjectCommand, PutObjectCommand,DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const {S3}  = require('../utility/config')
 
@@ -19,11 +19,34 @@ const getObjectURL = async(bucket,key) =>{
     return url ;
 }
 
+const putObjectUrl = async (bucket,contentType,filename) => {
+    const command = new PutObjectCommand({
+        Bucket: bucket,
+        Key : `profile-image/${filename}`,
+        ContentType : contentType
+    });
+    const url = await getSignedUrl(s3Client,command);
+    return url ;
+}
+
+
+const deleteObject = async(bucket,key) =>{
+    const command = new DeleteObjectCommand({
+        Bucket: bucket,
+        Key : key
+    });
+    const responce = s3Client.send(command);
+    return responce;
+}
+
 
 async function init (){
-    console.log('URL',await getObjectURL('prakash.jaiswal625','IMG_20221217_160930.jpg'))
+    // console.log('URL',await getObjectURL('prakash.jaiswal625','IMG_20221217_160930.jpg'))
+    // console.log('put URL' ,await putObjectUrl('prakash.jaiswal625','image/jpg',`image-${Date.now()}.jpg`) )
+    console.log('delete object' ,await deleteObject('prakash.jaiswal625','IMG_20221217_160930.jpg') )
+
 }
   
 init();
 
-module.exports = {getObjectURL}
+module.exports = {getObjectURL,putObjectUrl}
